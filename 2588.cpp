920f33aa39d9af12bba525943aa76b7dff61ba74
PJS <ziho2463@naver.com>
2020-12-17 02:02:12 +0900

    BOJ_2588

---
+++ 2588.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    long inputA = 0, inputB = 0;
+    long num[4];
+
+    cin >> inputA;
+    cin >> inputB;
+    num[3] = inputB;
+
+    for (int i = 2; i >= 0; i--) {
+        num[i] = inputB / pow(10, i);
+        inputB -= pow(10, i) * num[i];
+    }
+
+    for (int i = 0;i < 4;i++) {
+        cout << inputA * num[i] << endl;
+    }
+}
+
+
+
