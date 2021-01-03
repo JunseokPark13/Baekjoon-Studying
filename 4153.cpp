d18d0213fd5aa439802ac86e22dc254414b8674e
Suppplier <ziho2463@naver.com>
2021-01-04 00:29:25 +0900

    0104_boj

--- 4153.cpp
+++ 4153.cpp
@@ -5,10 +5,16 @@ using namespace std;
 bool tri_check(int a, int b, int c) {
 	bool check = false;
 
+	if (a * a + b * b == c * c) {
+		check = true;
+	}
+
 	return check;
 }
 
 int main() {
+
+	bool check = false;
 
 	while (1) {
 		int a, b, c; cin >> a >> b >> c;
@@ -16,13 +22,23 @@ int main() {
 
 		if (a >= b) {
 			if (a >= c) {
-
+				check = tri_check(b, c, a);
+			}
+			else {
+				check = tri_check(a,b,c);
 			}
 		}
-
-
+		else {
+			if (b >= c) {
+				check = tri_check(a, c, b);
+			}
+			else {
+				check = tri_check(a, b, c);
+			}
+		}
+		if (check) cout << "right" << "\n";
+		else cout << "wrong" << "\n";
 	}
-
 
 	return 0;
 }
