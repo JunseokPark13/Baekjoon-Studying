ed27c787995049ca01c1946c79743c5526c4d1cf
Suppplier <ziho2463@naver.com>
2021-01-11 00:52:07 +0900

    0110 prg

---
+++ programmers/완주하지못한 선수.js
@@ -0,0 +1,33 @@
+function solution(participant, completion) {
+    var answer = '';
+    
+    participant.sort();
+    completion.sort();
+    
+    console.log(participant);
+    console.log(completion);
+       
+    for(var i = 0; i<participant.length; i++){
+        if(participant[i]!=completion[i]){
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    
+    
+    return answer;
+}
+
+
+// function solution(participant, completion) {
+//     var answer = '';
+    
+//     for(var i=0; i<completion.length; i++){
+//         participant.splice(participant.indexOf(completion[i]),1);
+//         }
+//     answer = participant[0];
+    
+    
+//     return answer;
+// }
---
+++ programmers/크레인 인형뽑기 게임.js
@@ -0,0 +1,47 @@
+function printlog(ary, leng){
+    console.log('start');
+    for(var i = 0; i<leng; i++){
+        
+        console.log(i,"=",ary[i]);
+    }
+    console.log('end');
+}
+
+
+function solution(board, moves) {
+    var answer = 0;
+    var bucket = [];
+    var stack =0;
+       
+    for(var i = 0; i<moves.length; i++){
+        var k = moves[i]-1;
+        var pick = false;
+        for(var j=0; j<board[k].length; j++){
+            if(board[j][k]!=0 && !pick){
+                bucket[stack++] = board[j][k];
+                board[j][k]=0;
+                pick=true;
+            }
+        }
+    }
+    
+    var length = bucket.length
+    //printlog(bucket, length);
+    for(var i = 1; i<=length; i++){
+        if(length==2) console.log(i, bucket[i-1],bucket[i])
+           if(bucket[i-1]==bucket[i]){
+               
+               //console.log(i, 'check');
+               answer+=2;
+               for(var j = i+1; j<length; j++){
+                   bucket[j-2] = bucket[j];
+               }               
+               i=0;
+               length-=2;
+               //printlog(bucket, length);
+               //console.log("length = ", length);
+           }
+    }
+    
+    return answer;
+}
