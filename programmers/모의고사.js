13346d888e815abfc61e265daefe94b3e9bed40a
Suppplier <ziho2463@naver.com>
2021-01-12 00:28:10 +0900

    0111 prg boj

--- programmers/모의고사.js
+++ programmers/모의고사.js
@@ -8,18 +8,9 @@ function solution(answers) {
     var c1=0, c2=0, c3=0;
     
     for(var i = 0; i<answers.length; i++){
-        if(answers[i] == h1[n1]) {
-            console.log(answers[i], h1[n1]);
-            c1++;
-        }
-        if(answers[i] == h2[n2]) {
-            console.log(answers[i], h2[n2]);
-            c2++;
-        }
-        if(answers[i] == h3[n3]) {
-            console.log(answers[i], h3[n3]);
-            c3++;
-        }
+        if(answers[i] == h1[n1]) c1++;
+        if(answers[i] == h2[n2]) c2++;
+        if(answers[i] == h3[n3]) c3++;
         n1++; n2++; n3++;
         if(n1==5) n1=0;
         if(n2==8) n2=0;
@@ -31,11 +22,8 @@ function solution(answers) {
     if(max == c1) answer.push(1);
     if(max == c2) answer.push(2);
     if(max == c3) answer.push(3);
-
-    console.log(c1, c2, c3);
-
+    
+    
+    
     return answer;
 }
-
-
-//var t1 = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5];
