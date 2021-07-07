22d40d4ba30a1d82cb2e5c7b38ddc283d4367a4f
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-07-07 22:30:52 +0900

    0707_prg

--- programmers/test.js
+++ programmers/test.js
@@ -1,31 +1,49 @@
-function solution(n, k) {
-  var answer = [];
-  let arr = [];
-  for (let i = 1; i <= n; i++) arr.push(i);
-  let fac = arr.reduce((acc, val) => acc * val, 1);
-  k--;
-  while (answer.length !== n) {
-    let pos
-    fac = fac / arr.length;
-    pos = Math.floor(k / fac)
-    let temp = arr[pos];
-    answer.push(temp);
-    k = k % fac;
-    arr.splice(pos, 1)
+function solution(n, works) {
+  var answer = 0;
+
+  let worksSum = works.reduce((acc, val) => acc + val, 0)
+  works.sort((a, b) => b - a)
+
+  if (worksSum <= n) return 0
+
+  let pos = 0
+  while (n){
+    if (works[pos] < works[pos + 1]){
+      pos++
+    } else if (pos && works[pos - 1] === works[pos]){
+      pos = 0
+    } else{
+      works[pos]--
+      n--
+    }
   }
+
+  answer = works.reduce((acc, val) => acc + (val * val), 0)
+  
   return answer;
-} // 2021-07-05
-// https://juyoungpark718.github.io/posts/54
+} // 2021-07-07
 
-console.log("after  : ", solution(3, 5));
-console.log("answer : ", [3,1,2]);
+// https://programmers.co.kr/learn/courses/30/lessons/12927
+
+console.log("after  : ", solution(4, [4, 3, 3]));
+console.log("answer : ", 12);
 console.log("\n");
 
-console.log("after  : ", solution(5, 15));
-console.log("answer : ", );
+console.log("after  : ", solution(1, [2, 1, 2]));
+console.log("answer : ", 6);
 console.log("\n");
 
+console.log("after  : ", solution(3, [1, 1]));
+console.log("answer : ", 0);
+console.log("\n");
 
+console.log("after  : ", solution(5, [5, 3, 3, 2, 1]))
+console.log("answer : ", 17)
+console.log("\n");
+
+console.log("after  : ", solution(99, [2, 15, 22, 55, 55]))
+console.log("answer : ", 580)
+console.log("\n");
 
 
 
